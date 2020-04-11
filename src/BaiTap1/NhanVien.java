253ff4c7dddcfd9439c571bcb7b4cb84d70b6292
doanmelody2607 <63444961+doanmelody2607@users.noreply.github.com>
2020-04-11 15:53:30 +0700

    v2.0

---
+++ src/BaiTap1/NhanVien.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaiTap1;
+
+/**
+ *
+ * @author Vu Ngoc Doan
+ */
+public class NhanVien {
+    String ten;
+    int tuoi;
+    String diachi;
+    double luong;
+    int tonggio;
+    
+}
